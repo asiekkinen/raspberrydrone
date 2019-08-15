6c20d9768ac54c9209eca2020cbf8af2294f9ea6
Aku Siekkinen <aku.siekkinen@hotmail.com>
2019-08-15 16:15:13 +0300

    Removed unused teardown
    The teardown is done in main.py

--- flight_controller/flight_controller.py
+++ flight_controller/flight_controller.py
@@ -53,15 +53,6 @@ class FlightController:
             self._calculate_pulses()
             self._send_pulses()
 
-    def terminate(self):
-        """Terminate the flight controller.
-
-        Kills the pigpiod daemon and this in turn cuts the power to the escs.
-        """
-        output = str(subprocess.Popen("ps ax | grep pigpiod", shell=True, stdout=subprocess.PIPE).stdout.read())
-        pid = re.search(r"(?<= )[0-9]+", output).group(0)
-        subprocess.Popen("sudo kill {}".format(pid), shell=True)
-
     def _parse_commands(self, commands):
         """Handle inputted commands.
 
