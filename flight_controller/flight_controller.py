8d3a1643d76176916db78980e8ef560f5feb974a
Aku Siekkinen <aku.siekkinen@hotmail.com>
2019-08-17 20:33:47 +0300

    Added docs and changed methods and variables to private

--- flight_controller/flight_controller.py
+++ flight_controller/flight_controller.py
@@ -7,23 +7,41 @@ from drone.flight_controller.esc import ESC
 
 
 class FlightController:
-    """Flight controller for quadcopter."""
+    """Flight controller for quadcopter.
+
+    The motors are indexed as follows
+    0   1
+     \ /
+      |
+     / \
+    3   2
+    """
+
+    _min_roll = -30  # angle in degrees
+    _max_roll = 30  # angle in degrees
+    _min_pitch = -30  # angle in degrees
+    _max_pich = 30  # angle in degrees
+    _min_yaw = -90  # angular speed in degrees/sec
+    _max_yaw = 90  # angular speed in degrees/sec
+
+    # If throttle is set to higher than this value, then it is assumed that
+    # the drone is flying and the PID values are calculated.
+    _flying_threshold = 1100
 
     def __init__(self):
-        """Initialize variables."""
-        self.imu = None
-        self.errors = {"yaw": None, "pitch": None, "roll": None}
-        self.pids = {"yaw": PID(0, 0, 0), "pitch": PID(0, 0, 0), "roll": PID(0, 0, 0)}
-        self.pulses = [1000, 1000, 1000, 1000]
-        self.commands = {"throttle": 100, "yaw": 0, "pitch": 0, "roll": 0}
-        self.escs = [None, None, None, None]
-        self.flying = False
-        self.flying_threshold = 1100
+        self._imu = None
+        self._errors = {"yaw": None, "pitch": None, "roll": None}
+        self._pids = {"yaw": PID(0, 0, 0), "pitch": PID(0, 0, 0), "roll": PID(0, 0, 0)}
+        self._pulses = [1000, 1000, 1000, 1000]
+        self._commands = {"throttle": 100, "yaw": 0, "pitch": 0, "roll": 0}
+        self._escs = [None, None, None, None]
+        self._flying = False
 
     def setup(self):
         """Common setup wrapper.
 
-        Initialize IMU and the ESCs.
+        Initialize the inertial measurement unit and the electronic speed
+        controllers.
         """
         self._initialize_imu()
         self._initialize_escs()
@@ -31,10 +49,12 @@ class FlightController:
     def loop(self, queue):
         """Flight loop.
 
-        At each loop update orientation, read commands, calculate errors, PIDs 
+        At each loop update orientation, read commands, calculate errors, PIDs
         and pulses, and last send the pulses to the ESCs.
-        
-        Use multiprocessing.Queue to receive commands.
+
+        Use multiprocessing.Queue to receive commands. If there are no commands
+        recieved for a while, then it is assumed that something is wrong and an
+        error is raised.
 
         Parameters
         ----------
@@ -42,7 +62,7 @@ class FlightController:
         """
         no_message_counter = 0
         while True:
-            self.imu.update_orientation()
+            self._imu.update_orientation()
             if not queue.empty():
                 no_message_counter = 0
                 data = queue.get()
@@ -55,31 +75,31 @@ class FlightController:
             else:
                 no_message_counter += 1
                 if no_message_counter > 1000:
-                    raise AssertionError("No message recieved in 1000 epochs")
-            if self.flying:
+                    raise AssertionError("No message received in 1000 epochs")
+            if self._flying:
                 self._calculate_pids()
             else:
-                for pid in self.pids:
-                    self.pids[pid].pid = 0
+                for pid in self._pids:
+                    self._pids[pid].pid = 0
             self._calculate_pulses()
             self._send_pulses()
 
     def _parse_commands(self, commands):
         """Handle inputted commands.
 
-        Parse the commands and set the flying status. If throttle is set to 
+        Parse the commands and set the flying status. If throttle is set to
         greater than 1100, then it is assumed that the drone is airborne.
 
         Throttle's value stays intact.
 
-        Yaw is linearly transformed to between -90 and 90 corresponding to
-        angular speeds in deg/s
-        
-        Roll is linearly transformed to between -30 and 30 corresponding to 
-        orientation in the roll axis.
+        Yaw is linearly transformed to between self._min_yaw and self._max_yaw
+        corresponding to angular speeds in deg/s
 
-        Pitch is linearly transformed to between -30 and 30 corresponding to 
-        orientation in the pitch axis.
+        Roll is linearly transformed to between self._min_roll and
+        self._max_roll corresponding to orientation in the roll axis.
+
+        Pitch is linearly transformed to between self._min_pitch and
+        self._max_pitch corresponding to orientation in the pitch axis.
 
         Parameters
         ----------
@@ -89,22 +109,22 @@ class FlightController:
         """
         for key, value in commands.items():
             if key == "yaw":
-                self.commands[key] = -90 + (value - 1000) * 180 / 1000
+                self._commands[key] = -90 + (value - 1000) * 180 / 1000
             elif key == "roll":
-                self.commands[key] = -30 + (value - 1000) * 60 / 1000
+                self._commands[key] = -30 + (value - 1000) * 60 / 1000
             elif key == "pitch":
-                self.commands[key] = -30 + (value - 1000) * 60 / 1000
+                self._commands[key] = -30 + (value - 1000) * 60 / 1000
             elif key == "throttle":
-                self.commands[key] = value
-        if self.commands["throttle"] > self.flying_threshold:
-            self.flying = True
+                self._commands[key] = value
+        if self._commands["throttle"] > self._flying_threshold:
+            self._flying = True
         else:
-            self.flying = False
+            self._flying = False
 
     def _initialize_imu(self):
         """Connect and calibrate the inertial measurement unit."""
-        self.imu = IMU()
-        self.imu.calibrate()
+        self._imu = IMU()
+        self._imu.calibrate()
 
     def _initialize_escs(self, front_left_pin=18, front_right_pin=24,
                         back_right_pin=12, back_left_pin=13):
@@ -128,9 +148,9 @@ class FlightController:
         """
         subprocess.Popen("sudo pigpiod", shell=True)
         sleep(2)
-        self.escs = [ESC(front_left_pin), ESC(front_right_pin),
+        self._escs = [ESC(front_left_pin), ESC(front_right_pin),
                      ESC(back_right_pin), ESC(back_left_pin)]
-        for esc in self.escs:
+        for esc in self._escs:
             esc.set_pulsewidth(1000)
 
     def _calculate_pids(self):
@@ -138,27 +158,27 @@ class FlightController:
 
         The PID is calculated for each yaw, pitch and roll individually.
         """
-        self.errors["yaw"] = self.commands["yaw"] - self.imu.yaw
-        self.errors["pitch"] = self.commands["pitch"] - self.imu.pitch
-        self.errors["roll"] = self.commands["roll"] - self.imu.roll
-        for pid in self.pids:
-            self.pids[pid].calculate(self.errors[pid])
+        self._errors["yaw"] = self._commands["yaw"] - self._imu.yaw
+        self._errors["pitch"] = self._commands["pitch"] - self._imu.pitch
+        self._errors["roll"] = self._commands["roll"] - self._imu.roll
+        for pid in self._pids:
+            self._pids[pid].calculate(self._errors[pid])
 
     def _calculate_pulses(self):
         """Calculate the pulses to be send to the escs.
-        
+
         Uses the PID to calculate the pulsewidths.
         """
-        self.pulses[0] = int(self.commands["throttle"] + self.pids["roll"].pid
-                             + self.pids["pitch"].pid - self.pids["yaw"].pid)
-        self.pulses[1] = int(self.commands["throttle"] - self.pids["roll"].pid
-                             + self.pids["pitch"].pid + self.pids["yaw"].pid)
-        self.pulses[2] = int(self.commands["throttle"] - self.pids["roll"].pid
-                             - self.pids["pitch"].pid - self.pids["yaw"].pid)
-        self.pulses[3] = int(self.commands["throttle"] + self.pids["roll"].pid
-                             - self.pids["pitch"].pid + self.pids["yaw"].pid)
+        self._pulses[0] = int(self._commands["throttle"] + self._pids["roll"].pid
+                             + self._pids["pitch"].pid - self._pids["yaw"].pid)
+        self._pulses[1] = int(self._commands["throttle"] - self._pids["roll"].pid
+                             + self._pids["pitch"].pid + self._pids["yaw"].pid)
+        self._pulses[2] = int(self._commands["throttle"] - self._pids["roll"].pid
+                             - self._pids["pitch"].pid - self._pids["yaw"].pid)
+        self._pulses[3] = int(self._commands["throttle"] + self._pids["roll"].pid
+                             - self._pids["pitch"].pid + self._pids["yaw"].pid)
 
     def _send_pulses(self):
         """Change pulsewidths for the escs."""
         for i in range(4):
-            self.escs[i].set_pulsewidth(self.pulses[i])
+            self._escs[i].set_pulsewidth(self._pulses[i])
